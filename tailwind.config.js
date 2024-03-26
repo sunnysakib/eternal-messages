e22f820df8e3b3b3d4d59f1af932bef8d74baff5
sunnysakib <sakibur.rahaman033@gmail.com>
2024-03-26 23:31:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*", 'node_modules/preline/dist/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#002FD1',
+        'primary-20': '#D4DCFD',
+        'gray-2': '#D0D5DD',
+        'white': "#FCFCFD"
+      }
+    },
+  },
+  plugins: [require('preline/plugin'), require('@tailwindcss/forms')],
+}
+
