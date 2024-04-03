1964a0b5b01d23d93ec49f9005bdb1ca993e3e8b
sunnysakib <sakibur.rahaman033@gmail.com>
2024-04-03 11:42:14 +0600

    chat functinality added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         'primary': '#002FD1',
         'primary-20': '#D4DCFD',
         'gray-2': '#D0D5DD',
-        'white': "#FCFCFD"
+        'white': "#FCFCFD",
+        'green': '#26BF94'
       }
     },
   },
